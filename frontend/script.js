e8c68688ce8a88bd7248ba9f66a78baa2c9a30aa
Expl0sivePL <dominik200410@gmail.com>
2025-11-17 23:25:31 +0000

    Added loadMedicines script

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+async function loadMedicines() {
+    try {
+        const response = await fetch('http://localhost:3000/medicines');
+        const data = await response.json();
+
+        const listMedicine = document.getElementById('medicine-list');
+        listMedicine.innerHTML = '';
+
+        
+        data.medicines.forEach(med => {
+            list.innerHTML += `
+                <div>
+                    <strong>${med.name}</strong> - ${med.price ?? "No price available"}
+                </div>`;
+        });
+
+    } catch (err) {
+        console.error("Failed to fetch medicines:", err);
+        document.getElementById("medicine-list").innerHTML =
+            "<p style='color:red;'>Unable to load data.</p>";
+    }
+}
+
+loadMedicines();
